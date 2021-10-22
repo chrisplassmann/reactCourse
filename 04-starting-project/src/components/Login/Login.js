d7cab3a81d4e0f9f1055f54fb6219afe4b37cd64
chrisplassmann <chris.plassmann@nerdpower.energy>
2021-10-22 16:00:52 -0500

    daily

--- 04-starting-project/src/components/Login/Login.js
+++ 04-starting-project/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,19 +11,41 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    console.log('EFFECT RUNNING');
+
+    return () => {
+      console.log('EFFECT CLEANUP')
+    }
+  }, [enteredPassword])
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('checking validity')
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+  //   // clean up function
+  //   return () => {
+  //     console.log('cleanup');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword])
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+      event.target.value.includes('@') && enteredPassword.trim().length > 6    
     );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
+    
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+      enteredEmail.includes('@') && event.target.value.trim().length > 6    
     );
   };
 
